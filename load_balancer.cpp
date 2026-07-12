702a6b2fd784aceacf9bf95bb56bd1707bc45607
met9787 <metorresramos@gmail.com>
2026-07-12 03:10:53 -0500

    started load balancer code

--- load_balancer.cpp
+++ load_balancer.cpp
@@ -1,10 +1,36 @@
-#include <load_balancer.h>
+#include "load_balancer.h"
 
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 
+using std::queue;
 
+LoadBalancer::LoadBalancer(int num_servers, int t, queue<Request> q) {
+    threshold = t;
+    request_q = q;
+
+    for (int i = 0; i < num_servers; i++) {
+        servers.push_back(WebServer());
+    }
+}
+
+void LoadBalancer::cycle() {
+    for (int i = 0; i < servers.size(); i++) {
+
+        if (!servers[i].busy()) {
+
+            if (!request_q.empty()) {
+                servers[i].assign_request(request_q.front());
+                request_q.pop();
+            }
+
+        } else {
+            servers[i].cycle();
+        }
+    }
+    return;
+}
 
 
 
--- load_balancer.h
+++ load_balancer.h
@@ -6,6 +6,18 @@
 #include <queue>
 #include <vector>
 
+using std::queue, std::vector;
 
+class LoadBalancer {
+    private:
+        int threshold;
+        queue<Request> request_q;
+        vector<WebServer> servers;
+        
+    public:
+        LoadBalancer(int num_servers, int t, queue<Request> q);
+        void cycle();
+
+};
 
 
